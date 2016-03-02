3f3e867b64552b28fe40e42c9471036ceb638258
Nick Stewart <nick.stewart.programmer@gmail.com>
2016-03-01 19:13:51 -0700

    Changing layout of default settings
    Changing to two columns as the primary layout for the settings instead
    of rows.

--- app/default-settings.js
+++ app/default-settings.js
@@ -31,94 +31,71 @@ var defaultSettings = {
 
         m('.row', [
           // # of Bikes Select
-          m('.col-xs-12 col-sm-6', [
-            m('.input-group.inline', [
-              m('label', [
-                'Number of Bikes ',
-                m('select', {
-                  onchange: ctrl.onChange(function(event) {
-                    ctrl.settings.bikes = parseInt(event.target.value);
-                  })
-                },
-                ctrl.bikeOptions.map(function(option){
-                  return m('option', {
-                    selected: option === ctrl.settings.bikes,
-                    value: option
-                  }, option);
-                }))
+          m('.col-xs-12.col-sm-6', [
+
+            m('.row', [
+              m('.input-group.inline', [
+                m('label', [
+                  'Number of Bikes ',
+                  m('select', {
+                    onchange: ctrl.onChange(function(event) {
+                      ctrl.settings.bikes = parseInt(event.target.value);
+                    })
+                  },
+                  ctrl.bikeOptions.map(function(option){
+                    return m('option', {
+                      selected: option === ctrl.settings.bikes,
+                      value: option
+                    }, option);
+                  }))
+                ])
+              ])
+            ]),
+
+            m('.row', [
+              m('.input-group.inline', [
+                m('label', [
+                  'Roller Diameter ',
+                  m('input', {type: 'text'})
+                ]),
+                m('select', [
+                  m('option', 'in'),
+                  m('option', 'cm')
+                ])
               ])
             ])
+
           ]),
 
-          // Distance/Speed Select
-          m('.col-xs-12 col-sm-6', [
-            m('.input-group.inline', [
-              m('label', [
-                'Distance/Speed Units ',
-                m('select', {
-                  onchange: ctrl.onChange(function(event) {
-                    ctrl.settings.unitsOfMeasure = event.target.value;
-                  })
-                },
-                ctrl.unitsOfMeasureOptions.map(function(option) {
-                  return m('option', {
-                    selected: option.value === ctrl.settings.unitsOfMeasure,
-                    value: option.value
-                  }, option.label);
-                }))
-              ])
-            ])
-          ])
-        ]),
+          m('.col-xs-12.col-sm-6', [
 
-        m('.row', [
-          m('.col-xs-12 col-sm-6', [
-            m('.input-group.inline', [
-              m('label', [
-                'Roller Diameter ',
-                m('input', {type: 'text'})
-              ]),
-              m('select', [
-                m('option', 'in'),
-                m('option', 'cm')
+            m('.row', [
+              m('.input-group.inline', [
+                m('label', [
+                  'Distance/Speed Units ',
+                  m('select', {
+                    onchange: ctrl.onChange(function(event) {
+                      ctrl.settings.unitsOfMeasure = event.target.value;
+                    })
+                  },
+                  ctrl.unitsOfMeasureOptions.map(function(option) {
+                    return m('option', {
+                      selected: option.value === ctrl.settings.unitsOfMeasure,
+                      value: option.value
+                    }, option.label);
+                  }))
+                ])
+              ])
+            ]),
+
+            m('.row', [
+              m('.input-group.inline', [
+                m('label', [
+                  'Race Distance ',
+                  m('input', {type: 'number'})
+                ])
               ])
             ])
-          ]),
-          m('.col-xs-12 col-sm-6', [
-            m('.input-group.inline', [
-              m('label', [
-                'Race Distance ',
-                m('input', {type: 'number'})
-              ])
-            ])
-          ])
-        ]),
-        m('.row', [
-          m('.col-xs-12 col-sm-6', [
-            m('.input-group.inline',
-              m('label.control-label', [
-                'Circumference ',
-                m('span', ctrl.circumference())
-              ])
-            )
-          ]),
-          //m('.col-xs-12 col-sm-6', [
-          //  m('.input-group.inline',
-          //    m('label.control-label', [
-          //      'Time Display ',
-          //      m('span', '????')
-          //    ])
-          //  )
-          //])
-        ]),
-        m('.row', [
-          m('.col-xs-12', [
-            m('.input-group',
-              m('label.control-label', [
-                'Winner\'s Message (max 25 characters) ',
-                m('input', {type: 'text'})
-              ])
-            )
           ])
         ])
       ])
