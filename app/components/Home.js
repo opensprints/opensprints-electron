6119311e954cf6cb23fbb96aaae2caba7f0196b4
Nick Stewart <nick.stewart.programmer@gmail.com>
2016-07-20 01:35:03 -0600

    Removing Counter component from being referenced.

--- app/components/Home.js
+++ app/components/Home.js
@@ -9,7 +9,6 @@ export default class Home extends Component {
       <div>
         <div className={styles.container}>
           <h2>Home</h2>
-          <Link to="/counter">to Counter</Link>
         </div>
       </div>
     );
--- app/reducers/index.js
+++ app/reducers/index.js
@@ -1,11 +1,9 @@
 import { combineReducers } from 'redux';
 import { routerReducer as routing } from 'react-router-redux';
-import counter from './counter';
 import navigationVisible from './navigationVisible';
 import defaultSettings from './defaultSettings';
 
 const rootReducer = combineReducers({
-  counter,
   navigationVisible,
   defaultSettings,
   routing
