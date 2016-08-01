3200e46c3d1715e5d9d8269eaf6e3788874976eb
Nick Stewart <nick.stewart.programmer@gmail.com>
2016-07-31 21:49:50 -0600

    Removing most of the old mithril code after converting the pieces needed to get back to where we were before.

--- app/routes.js
+++ app/routes.js
@@ -2,37 +2,14 @@ import React from 'react';
 import { Route, IndexRoute } from 'react-router';
 import App from './containers/App';
 import HomePage from './containers/HomePage';
+import RosterPage from './containers/RosterPage';
 import DefaultSettingsPage from './containers/DefaultSettingsPage';
 
 
 export default (
   <Route path="/" component={App}>
     <IndexRoute component={HomePage} />
+    <Route path="/roster" component={RosterPage} />
     <Route path="/default-settings" component={DefaultSettingsPage} />
   </Route>
 );
-
-// m.route.mode = 'pathname';
-//
-// var index = {
-//   controller: function() {
-//     window.m = m;
-//   },
-//   view: function() {
-//     return m('', [
-//       m.component(Header),
-//       m('.container', [
-//         m.component(QuickRace),
-//         m('.btn.round-btn', 'Race Again'),
-//         m('.btn.solid-round-btn', 'Next Race')
-//       ])
-//     ]);
-//   }
-// };
-//
-// m.route(document.body, '/', {
-//   '/': index,
-//   '/default-settings': DefaultSettings,
-//   '/roster': Roster,
-//   '/race-results': RaceResults
-// });
